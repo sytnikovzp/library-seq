28e8c8717b99123ed633ffb181a18c677dd1d70f
alex <sytnikov.zp@gmail.com>
2024-07-10 22:30:31 +0300

    Generate model Book

---
+++ src/db/migrations/20240710192521-create-book.js
@@ -0,0 +1,44 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Books', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      title: {
+        allowNull: false,
+        unique: true,
+        type: Sequelize.STRING,
+      },
+      genre: {
+        allowNull: false,
+        unique: true,
+        type: Sequelize.STRING,
+      },
+      shelf: {
+        allowNull: false,
+        unique: true,
+        type: Sequelize.STRING,
+      },
+      description: {
+        type: Sequelize.TEXT,
+      },
+      createdAt: {
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        type: Sequelize.DATE,
+      },
+      image: {
+        type: Sequelize.STRING,
+      },
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Books');
+  },
+};
---
+++ src/db/models/book.js
@@ -0,0 +1,40 @@
+'use strict';
+const { Model } = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Book extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Book.init(
+    {
+      title: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+      },
+      genre: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+      },
+      shelf: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+      },
+      description: DataTypes.TEXT,
+      image: DataTypes.STRING,
+    },
+    {
+      sequelize,
+      modelName: 'Book',
+    }
+  );
+  return Book;
+};
