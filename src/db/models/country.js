20f18ff72cb1ded1ee6b147ae2641a7cdba02817
alex <sytnikov.zp@gmail.com>
2024-07-10 21:57:35 +0300

    Generate model Country

---
+++ src/db/models/country.js
@@ -0,0 +1,28 @@
+'use strict';
+const { Model } = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Country extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Country.init(
+    {
+      title: {
+        type: DataTypes.STRING,
+        unique: true,
+      },
+      description: DataTypes.TEXT,
+    },
+    {
+      sequelize,
+      modelName: 'Country',
+    }
+  );
+  return Country;
+};
