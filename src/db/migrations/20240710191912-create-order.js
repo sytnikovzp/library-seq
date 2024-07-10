cc630cceb9969f6b8d6a558a6b369a82eb1a7b27
alex <sytnikov.zp@gmail.com>
2024-07-10 22:23:05 +0300

    Generate model Order

---
+++ src/db/migrations/20240710191912-create-order.js
@@ -0,0 +1,33 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Orders', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      title: {
+        unique: true,
+        type: Sequelize.STRING,
+      },
+      order_date: {
+        type: Sequelize.DATE,
+      },
+      customer: {
+        type: Sequelize.STRING,
+      },
+      createdAt: {
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Orders');
+  },
+};
---
+++ src/db/models/order.js
@@ -0,0 +1,29 @@
+'use strict';
+const { Model } = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Order extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Order.init(
+    {
+      title: {
+        type: DataTypes.STRING,
+        unique: true,
+      },
+      order_date: DataTypes.DATE,
+      customer: DataTypes.STRING,
+    },
+    {
+      sequelize,
+      modelName: 'Order',
+    }
+  );
+  return Order;
+};
